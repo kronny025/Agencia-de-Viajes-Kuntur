3aa85e85e222f8816a0119888600bae34ef1a55f
kronny025 <104878551+kronny025@users.noreply.github.com>
2022-06-20 16:31:05 -0500

    Add files via upload

---
+++ java/capanegocio/Pago.java
@@ -0,0 +1,24 @@
+package capanegocio;
+
+public class Pago {
+    // atributos
+    private String tipoPago;
+    private String cantidad;
+
+    public String getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+}
